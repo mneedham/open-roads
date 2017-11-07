a2ab332a397850a6659c762f4ae954cb45f89073
Mark Needham <mark.needham@neotechnology.com>
2017-11-07 20:25:39 +0000

    don't need two versions of the route

--- app/app.py
+++ app/app.py
@@ -68,104 +68,6 @@ def lookup_route(route_id):
                                    )
 
 
-generate_route_query = """\
-MATCH (middle1:Road)
-WHERE {latLow} < middle1.latitude < {latHigh}
-AND   {longLow} < middle1.longitude < {longHigh}
-AND SIZE((middle1)-[:CONNECTS]-()) > 1
-
-MATCH (middle2:Road)
-WHERE {latLow} + ((({latMetres}-{latVariability})/3) * 0.0000089)
-      < middle2.latitude <
-      {latHigh} + ((({latMetres}+{latVariability})/3) * 0.0000089)
-AND   {longLow} + ((({longMetres}-{longVariability})*3) * 0.0000089 / cos({lat} + (({latMetres}+100) * 0.0000089) * 0.018))
-      < middle2.longitude <
-      {longHigh} + ((({longMetres}+{longVariability})*3) * 0.0000089 / cos({lat} + (({latMetres}+100) * 0.0000089) * 0.018))
-AND SIZE((middle2)-[:CONNECTS]-()) > 1
-
-WITH middle1, middle2  WHERE middle1 <> middle2
-MATCH (start:Road {latitude: {lat}, longitude: {long}})
-WITH start, middle1, middle2
-ORDER BY rand()
-CALL roads.findMeARoute2(start, middle1, middle2)
-YIELD path
-WITH start, middle1, middle2,
-     nodes(path) as roads,
-     relationships(path) as connections
-LIMIT 1
-MERGE (route:Route { points: [road in roads | road.latitude + "," + road.longitude] })
-ON CREATE SET route.id =  apoc.create.uuid()
-SET route.start = [start.latitude, start.longitude],
-    route.middle1 = [middle1.latitude, middle1.longitude],
-    route.middle2 = [middle2.latitude, middle2.longitude],
-    route.distance = reduce(acc=0, connection in connections | acc + connection.length ),
-    route.estimatedDistance = {estimatedDistance},
-    route.direction = {direction}
-
-return route.id AS routeId
-"""
-
-
-@app.route('/routes', methods=['POST'])
-def routes():
-    if request.method == "POST":
-        estimated_distance = request.form.get('estimatedDistance')
-        estimated_distance = int(estimated_distance) if estimated_distance else 5000
-
-        direction = request.form.get('direction')
-        adjustment = 1 if direction == "north" else -1
-
-        lats = sorted([(estimated_distance / 5) * adjustment,
-                       (estimated_distance / 4) * adjustment])
-        lat_metres = random.randint(lats[0], lats[1])
-
-        lats_var = sorted(
-            [int(abs(lat_metres / 10)), int(abs(lat_metres / 8))])
-        lat_variability = random.randint(lats_var[0], lats_var[1])
-
-        long_metres = 100
-        long_variability = 200
-
-        lat = 51.357397146246264
-        lon = -0.20153965352074504
-
-        lat_low = lat + ((lat_metres - lat_variability) * 0.0000089)
-        lat_high = lat + ((lat_metres + lat_variability) * 0.0000089)
-
-        long_low = lon + ((long_metres - long_variability)
-                          * 0.0000089 / math.cos(lat * 0.018))
-        long_high = lon + ((long_metres + long_variability)
-                           * 0.0000089 / math.cos(lat * 0.018))
-
-        with driver.session() as session:
-            start = int(round(time.time() * 1000))
-            result = session.run(generate_route_query, {
-                "lat": lat,
-                "long": lon,
-
-                "latLow": lat_low,
-                "latHigh": lat_high,
-
-                "longLow": long_low,
-                "longHigh": long_high,
-
-                "latMetres": lat_metres,
-                "latVariability": lat_variability,
-
-                "longMetres": long_metres,
-                "longVariability": long_variability,
-                "direction": direction,
-                "estimatedDistance": estimated_distance
-            })
-            row = result.peek()
-            end = int(round(time.time() * 1000))
-            route_id = row["routeId"]
-            print("Route {id} generated in {time}".format(
-                id=route_id, time=(end - start)))
-
-        return redirect(url_for('lookup_route', route_id=route_id))
-
-
 generate_route_query2 = """\
 MATCH (middle1:Road)
 WHERE {middle1LatLow} < middle1.latitude < {middle1LatHigh}
@@ -200,8 +102,8 @@ return route.id AS routeId
 """
 
 
-@app.route('/routes2', methods=['POST'])
-def routes2():
+@app.route('/routes', methods=['POST'])
+def routes():
     if request.method == "POST":
         estimated_distance = request.form.get('estimatedDistance')
         estimated_distance = int(estimated_distance) if estimated_distance else 5000
